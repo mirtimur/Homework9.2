ed520b506591195c135f2116026d66b58eca9a5a
Mir TImur <mirtimur1@gmail.com>
2023-12-23 16:50:52 +0300

    Initial commit

---
+++ Homework9/Homework9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework9
+//
+//  Created by Timur Mir on 23.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
